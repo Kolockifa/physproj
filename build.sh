1df8a04e2056c1fb31f0765a9cf0566739d43e6e
Kolockifa <nikolajgrom64@outlook.com>
2026-02-23 19:38:16 +0300

    fix error

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 set -o errexit
 
 mix deps.get --only prod
+MIX_ENV=prod mix deps.compile
 MIX_ENV=prod mix compile
-MIX_ENV=prod mix assets.build
-MIX_ENV=prod mix assets.deploy
-
-MIX_ENV=prod mix phx.gen.release
-MIX_ENV=prod mix release --overwrite
